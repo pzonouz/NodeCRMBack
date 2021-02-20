eb92e8e55b945e29604ca9c409ff991aafa5f02b
Peyman <p.zonouz@gmail.com>
2021-02-20 09:12:59 +0330

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000, '0.0.0.0');
+  const port = process.env.PORT ? +process.env.PORT - 100 : 3000;
+  await app.listen(port | 3000, '0.0.0.0');
 }
 bootstrap();
