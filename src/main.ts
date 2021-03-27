afc7002a8d4a96df5deb9bb504bda91e8c90a1f0
Peyman <p.zonouz@gmail.com>
2021-03-27 13:43:02 +0430

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = process.env.PORT ? +process.env.PORT - 100 : 3000;
-  await app.listen(port | 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
